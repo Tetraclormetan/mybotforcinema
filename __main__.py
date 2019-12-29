3aa97c748b7aa36e3e78188045d56d2b28daeaa6
Alex <aliaksandr.shcharbitski@gmail.com>
2019-12-28 16:10:43 -0800

    message

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from imdb import IMDb, IMDbError
+from kinopoisk.movie import Movie
 # import wikipedia
 # import urllib
 # from bs4 import BeautifulSoup
@@ -13,21 +14,44 @@ from imdb import IMDb, IMDbError
 # wikipedia.set_lang("ru")
 
 
+
+
+'''
+'airing', 'akas', 'alternate versions', 'awards',
+ 'connections', 'crazy credits', 'critic reviews',
+  'episodes', 'external reviews', 'external sites',
+   'faqs', 'full credits', 'goofs', 'keywords',
+    'locations', 'main', 'misc sites', 'news',
+     'official sites', 'parents guide',
+      'photo sites', 'plot', 'quotes',
+       'recommendations', 'release dates',
+        'release info', 'reviews', 'sound clips',
+         'soundtrack', 'synopsis', 'taglines',
+          'technical', 'trivia', 'tv schedule',
+           'video clips', 'vote details'''
+
+
 bot = Bot(token='1067437658:AAGcrdziqbljDoeBFSDiPYxRJjFQHz5E-To')
 dp = Dispatcher(bot)
 ia = IMDb()
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message) -> None:
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.\n Type /movie")
 
 
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message) -> None:
+    await message.reply(ia.get_movie_infoset())
+
+
 @dp.message_handler(commands=["movie"])
 async def echo(message: types.Message) -> None:
+    to_find = str(message.text[7:])
     try:
         await message.answer("searching movie...")
-        to_find = str(message.text[7:])
+
 
         # st = str(wikipedia.search(to_find))
         # serch_res = wikipedia.search(st)
@@ -37,37 +61,81 @@ async def echo(message: types.Message) -> None:
 
         # await message.answer(st)
 
-        movies = ia.search_movie(message.text[7:])
-        await message.reply("title: " + str(movies[0]["title"]) + "\nyear: " + str(movies[0]["year"]) + "\n" +
-        #                    str(movies[0]['kind']) +
-                            "\n")
+        movies = ia.search_movie(to_find)
+        await message.answer("title: " + str(movies[0]["title"]))
+        if "year" in movies[0]:
+            await message.answer("\nyear: " + str(movies[0]["year"]))
+        if "kind" in movies[0]:
+            await message.answer("" + str(movies[0]["kind"]))
         await message.answer(movies[0]["cover url"])
-        await message.reply("searching additional info...")
+        await message.answer("searching additional info...")
         ans = movies[0].getID()
-        res = ia.get_movie(ans, info=['quotes', 'plot', 'awards', "full credits"])
-        await message.reply("processing info...")
+        res = ia.get_movie(ans, info=['quotes', 'plot', 'awards', "full credits", "video clips"])
+        await message.answer("processing info...")
         quotes = res.get('quotes', [])
         plot = res.get("plot")
         cred = res.get("cast")
         awards = res.get("awards", [])
-        # movie = ia.get_movie('0133093', info=['full credits'])  # Matrix
-        # assert 'cast' in movie
-        await message.reply("plot: " + str(plot[0]))
+        a = res.get("video clips")
+
+        #if "genres" in res:
+        #    await message.answer("genres: ", " ".join(res["genres"]))
+        await message.answer(type(a))
+        if a:
+            await message.answer_video(a[0])
+        if plot:
+            await message.answer("plot: " + str(plot[0]))
         if cred:
-            await message.reply("credits: \n " + str("\n".join([str(cred[i]) for i in range(min(len(cred), 5))])))
+            await message.answer("credits: \n " + str("\n".join([str(cred[i]["name"]) + " -- " + str(cred[i].currentRole) for i in range(min(len(cred), 5))])))
         if len(awards) > 0:
             awards = [str(x["award"]) + " " + str(x["year"]) + " " + (str(x["notes"] if "notes" in x else ""))
                       for x in awards]
             res_aw = "\n".join(awards[0: min(5, len(awards))])
-            await message.reply("awards: \n" + res_aw)
+            await message.answer("awards: \n" + res_aw)
         if len(quotes) > 0:
-            await message.reply("best quote: \n " + str(quotes[0][0][:min(len(quotes[0][0]), 150)] +
+            await message.answer("best quote: \n " + str(quotes[0][0][:min(len(quotes[0][0]), 150)] +
                                                         ("..." if (len(quotes[0][0]) > 150) else "")))
     except IMDbError as e:
         await message.reply('try another')
     except Exception as ex:
-        await message.reply("Try smth else")
+        await message.reply(str(ex))
+    try:
+        await message.answer("Now kinopoisk")
+        movie_list = Movie.objects.search(to_find)
+        m = movie_list[0]
+        m.get_content('main_page')
+        await message.answer(m.year)
+        await message.answer(m.title)
+        await message.answer(m.plot)
+        await message.answer(m.runtime)
+        await message.answer(m.tagline)
+        await message.answer(m.rating)
+        await message.answer(m.imdb_rating)
+        m.get_content('posters')
+        if m.posters:
+            await message.answer_photo(m.posters[0])
+
+    except Exception as ex:
+        await message.reply("kinopoisk wrong")
 
 
 if __name__ == '__main__':
     executor.start_polling(dp)
+
+
+'''
+
+julia = i.get_person('0000210')
+for job in julia['filmography'].keys():
+    print('# Job: ', job)
+    for movie in julia['filmography'][job]:
+        print('\t%s %s (role: %s)' % (movie.movieID, movie['title'], movie.currentRole))
+        
+last = julia['filmography']['actress'][0]
+# Retrieve full information
+i.update(last)
+# name of the first director
+print(last['director'][0]['name'])
+
+
+'''
