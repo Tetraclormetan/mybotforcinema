7123f2333c1806c693e1e337644caa7f12f487d5
Alex <aliaksandr.shcharbitski@gmail.com>
2019-12-30 08:18:24 -0800

    info

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-
 import os
 
 from aiogram import Bot, types
@@ -6,14 +5,14 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from imdb import IMDb, IMDbError
 from kinopoisk.movie import Movie
+import re
+
 # import wikipedia
 # import urllib
 # from bs4 import BeautifulSoup
 
 
 # wikipedia.set_lang("ru")
-
-
 
 
 '''
@@ -30,7 +29,6 @@ from kinopoisk.movie import Movie
           'technical', 'trivia', 'tv schedule',
            'video clips', 'vote details'''
 
-
 bot = Bot(token='1067437658:AAGcrdziqbljDoeBFSDiPYxRJjFQHz5E-To')
 dp = Dispatcher(bot)
 ia = IMDb()
@@ -38,91 +36,131 @@ ia = IMDb()
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message) -> None:
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.\n Type /movie")
+    await message.reply("Type /movie <title> \n or /actor <full name>")
 
 
 @dp.message_handler(commands=['help'])
 async def send_welcome(message: types.Message) -> None:
-    await message.reply(ia.get_movie_infoset())
+    # await message.reply(ia.get_movie_infoset())
+    await message.reply("Type /movie <title> \n or /actor <full name>")
 
 
 @dp.message_handler(commands=["movie"])
 async def echo(message: types.Message) -> None:
     to_find = str(message.text[7:])
     try:
-        await message.answer("searching movie...")
-
-
-        # st = str(wikipedia.search(to_find))
-        # serch_res = wikipedia.search(st)
-        # found = wikipedia.search(st)[0]
-        # if found:
-        #    await message.reply(str(found))
-
-        # await message.answer(st)
+        await message.answer("searching movie imdb...")
 
         movies = ia.search_movie(to_find)
-        await message.answer("title: " + str(movies[0]["title"]))
-        if "year" in movies[0]:
-            await message.answer("\nyear: " + str(movies[0]["year"]))
-        if "kind" in movies[0]:
-            await message.answer("" + str(movies[0]["kind"]))
-        await message.answer(movies[0]["cover url"])
-        await message.answer("searching additional info...")
-        ans = movies[0].getID()
-        res = ia.get_movie(ans, info=['quotes', 'plot', 'awards', "full credits", "video clips"])
-        await message.answer("processing info...")
-        quotes = res.get('quotes', [])
-        plot = res.get("plot")
-        cred = res.get("cast")
-        awards = res.get("awards", [])
-        a = res.get("video clips")
+        if movies:
+            raise Exception("skip")
+            s = ""
+            if "title" in movies[0]:
+                s += "\ntitle: " + str(movies[0]["title"])
+            if "year" in movies[0]:
+                s += "\nyear: " + str(movies[0]["year"])
+            if "kind" in movies[0]:
+                s += "\n" + str(movies[0]["kind"])
+            if s:
+                await message.answer("main info:\n" + s)
 
-        #if "genres" in res:
-        #    await message.answer("genres: ", " ".join(res["genres"]))
-        await message.answer(type(a))
-        if a:
-            await message.answer_video(a[0])
-        if plot:
-            await message.answer("plot: " + str(plot[0]))
-        if cred:
-            await message.answer("credits: \n " + str("\n".join([str(cred[i]["name"]) + " -- " + str(cred[i].currentRole) for i in range(min(len(cred), 5))])))
-        if len(awards) > 0:
-            awards = [str(x["award"]) + " " + str(x["year"]) + " " + (str(x["notes"] if "notes" in x else ""))
-                      for x in awards]
-            res_aw = "\n".join(awards[0: min(5, len(awards))])
-            await message.answer("awards: \n" + res_aw)
-        if len(quotes) > 0:
-            await message.answer("best quote: \n " + str(quotes[0][0][:min(len(quotes[0][0]), 150)] +
-                                                        ("..." if (len(quotes[0][0]) > 150) else "")))
+            await message.answer("searching additional info...")
+
+            ans = movies[0].getID()
+            res = ia.get_movie(ans, info=['quotes', 'plot', 'awards', "full credits", "video clips", "main", 'reviews',
+                                          ])
+
+            quotes = res.get('quotes', [])
+            plot = res.get("plot")
+            cred = res.get("cast")
+            awards = res.get("awards", [])
+            poster = res.get("cover url")
+            video_ref = res.get("video clips and trailers", [])
+            r = res.get("rating")
+            revs = res.get('reviews', [])
+
+            if r:
+                await message.answer("rating: " + str(r))
+
+            if plot:
+                await message.answer("plot: " + str(plot[0]))
+            if cred:
+                await message.answer("credits:\n " + str("\n".join([str(cred[i]["name"]) +
+                                                                    " -- " + str(cred[i].currentRole) for i in
+                                                                    range(min(len(cred), 5))])))
+            if len(awards) > 0:
+                awards = [str(x["award"]) + " " + str(x["year"]) + " " + (str(x["notes"] if "notes" in x else ""))
+                          for x in awards]
+                res_aw = "\n".join(awards[0: min(5, len(awards))])
+                await message.answer("awards: \n" + res_aw)
+
+            if len(quotes) > 0:
+                await message.answer("best quote: \n " + str(quotes[0][0][:min(len(quotes[0][0]), 150)] +
+                                                             ("..." if (len(quotes[0][0]) > 150) else "")))
+
+            if len(revs) > 0:
+                rev = ["review " + str(i + 1) + ") :" + str(revs[i]["content"][: min(300, len(revs[i]["content"]))])
+                       + ("..." if (len(revs[i]["content"]) > 300) else "") for i in range(min(5, len(revs)))]
+                await message.answer("top reviews:\n" + "\n".join(rev))
+
+            await message.answer("processing poster...")
+            if poster:
+                await message.answer_photo(poster)
+
+            if video_ref:
+                ur = video_ref[0][1]
+                ur = "".join(ur.split("\t"))
+                ur = "".join(ur.split("\n"))
+                await message.answer("smth like trailer:")
+                await message.answer(ur)
+        else:
+            await message.answer("nothing found")
     except IMDbError as e:
-        await message.reply('try another')
+        await message.reply('try another (c) IMDB')
     except Exception as ex:
         await message.reply(str(ex))
     try:
-        await message.answer("Now kinopoisk")
+        await message.answer("Kinopoisk")
         movie_list = Movie.objects.search(to_find)
+        if len(movie_list) == 0:
+            await message.answer("nothing found")
+            return None
         m = movie_list[0]
         m.get_content('main_page')
-        await message.answer(m.year)
-        await message.answer(m.title)
-        await message.answer(m.plot)
-        await message.answer(m.runtime)
-        await message.answer(m.tagline)
-        await message.answer(m.rating)
+        if hasattr(m, 'title'):
+            await message.answer("title: " + m.title)
+        if hasattr(m, 'year'):
+            await message.answer(m.year)
+        if hasattr(m, 'plot'):
+            await message.answer("plot:\n" + m.plot)
+        if hasattr(m, 'rating'):
+            await message.answer("rating " + str(m.rating))
         if m.imdb_rating:
-            await message.answer(m.imdb_rating)
+            await message.answer("imdb rating: " + str(m.imdb_rating))
+        if hasattr(m, "actors"):
+            await message.answer("actors: \n" + "\n".join(str(x) for x in m.actors[: min(len(m.actors), 5)]))
+        # await message.answer(m.runtime)
+        # await message.answer(m.tagline)
+        await message.answer("wait for large data...")
         m.get_content('posters')
         if m.posters:
             await message.answer_photo(m.posters[0])
+        m.get_content('trailers')
+        if m.trailers:
+            await message.answer(str(dir(m.trailers[0])))
+            await message.answer(str(m.trailers[0].file[:min(50, len(m.trailers[0].file))]))
+            await message.answer(str(m.trailers[0].is_valid))
 
+            #file = m.trailers[0].file
+            #await message.answer_video(file)
+        else:
+            await message.answer("no trailer, sorry")
     except Exception as ex:
         await message.reply(str(ex))
 
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-
 
 '''
 
