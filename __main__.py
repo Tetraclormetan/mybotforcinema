66f221f65776daea36399569c607e476f86abed8
Alex <aliaksandr.shcharbitski@gmail.com>
2019-12-30 11:40:33 -0800

    info

--- __main__.py
+++ __main__.py
@@ -4,35 +4,12 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from imdb import IMDb, IMDbError, Person
-# from kinopoisk.person import Person
 from kinopoisk.movie import Movie
 from google import google
 import re
 import urllib.request
 import urllib.parse
 import pytube
-
-# import wikipedia
-# import urllib
-# from bs4 import BeautifulSoup
-
-
-# wikipedia.set_lang("ru")
-
-
-'''
-'airing', 'akas', 'alternate versions', 'awards',
- 'connections', 'crazy credits', 'critic reviews',
-  'episodes', 'external reviews', 'external sites',
-   'faqs', 'full credits', 'goofs', 'keywords',
-    'locations', 'main', 'misc sites', 'news',
-     'official sites', 'parents guide',
-      'photo sites', 'plot', 'quotes',
-       'recommendations', 'release dates',
-        'release info', 'reviews', 'sound clips',
-         'soundtrack', 'synopsis', 'taglines',
-          'technical', 'trivia', 'tv schedule',
-           'video clips', 'vote details'''
 
 bot = Bot(token='1067437658:AAGcrdziqbljDoeBFSDiPYxRJjFQHz5E-To')
 dp = Dispatcher(bot)
@@ -53,7 +30,7 @@ async def send_welcome(message: types.Message) -> None:
 async def echo(message: types.Message) -> None:
     to_find = str(message.text[7:])
     await message.answer("got task")
-    
+
     # try:
     # query_string = urllib.parse.urlencode({"search_query": to_find + " trailer"})
     # html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
@@ -84,7 +61,6 @@ async def echo(message: types.Message) -> None:
 
         movies = ia.search_movie(to_find)
         if movies:
-            # raise Exception("skip")
             s = ""
             if "title" in movies[0]:
                 s += "\ntitle: " + str(movies[0]["title"])
@@ -171,20 +147,14 @@ async def echo(message: types.Message) -> None:
             await message.answer("imdb rating: " + str(m.imdb_rating))
         if hasattr(m, "actors"):
             await message.answer("actors: \n" + "\n".join(str(x) for x in m.actors[: min(len(m.actors), 5)]))
-        # await message.answer(m.runtime)
-        # await message.answer(m.tagline)
         await message.answer("wait for large data...")
         m.get_content('posters')
         if m.posters:
             await message.answer_photo(m.posters[0])
         m.get_content('trailers')
         if m.trailers and m.trailers[0].is_valid:
-            # await message.answer(str(dir(m.trailers[0])))
-            # await message.answer(str(m.trailers[0].file[:min(50, len(m.trailers[0].file))]))
-            await message.answer("https://www.kinopoisk.ru/trailer/player/share/" + str(m.trailers[0].id) + "/?share=true")
-
-            #file = m.trailers[0].file
-            #await message.answer_video(file)
+            await message.answer("https://www.kinopoisk.ru/trailer/player/share/" +
+                                 str(m.trailers[0].id) + "/?share=true")
         else:
             await message.answer("no trailer, sorry")
         await message.answer("end of processing")
@@ -219,23 +189,6 @@ async def echo(message: types.Message) -> None:
         await message.answer(str(ex))
 
 
-
 if __name__ == '__main__':
     executor.start_polling(dp)
 
-'''
-
-julia = i.get_person('0000210')
-for job in julia['filmography'].keys():
-    print('# Job: ', job)
-    for movie in julia['filmography'][job]:
-        print('\t%s %s (role: %s)' % (movie.movieID, movie['title'], movie.currentRole))
-        
-last = julia['filmography']['actress'][0]
-# Retrieve full information
-i.update(last)
-# name of the first director
-print(last['director'][0]['name'])
-
-
-'''
